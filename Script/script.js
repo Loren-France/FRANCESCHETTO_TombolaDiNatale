5023e7499178dac14e602c3a67d6f21995558318
Loren-France <14529846@itisrossi.vi.it>
2025-12-27 14:44:33 +0100

    Creazione input e numeri casuali, ma assegnazione undefined

--- Script/script.js
+++ Script/script.js
@@ -1,20 +1,38 @@
-function generaTabella() {
-    let tabella=[];
-    let righe="";
+function Start() {
     let griglia=document.getElementById("tabella");
     griglia.innerHTML="";
+
+    let output=document.getElementById("output");
+    output.innerHTML="";
+
+    let num=ArrayNumeri();
+
+    CreaTabella(num, griglia);
+
+    CreaInput(output);
+
+}
+
+function ArrayNumeri() {
     let numeri=[];
     let rand=0;
 
-    for (let k=1; k<=90; k++) {
+    for (let i=1; i<=90; i++) {
         rand=Math.floor(Math.random()*90)+1;
-        for (let l=0; l<numeri.length; l++) {
+        for (let j=0; j<numeri.length; j++) {
             if (rand==numeri[l]) {
-                rand=Math.floor(Math.random()*90)+1;
+                rand=parseInt(Math.floor(Math.random()*90)+1);
             }
             else {}
         }
     }
+    return numeri;
+}
+
+function CreaTabella(num, griglia) {
+    let tabella=[];
+    let righe;
+    let c=0;
 
     for (let i=0; i<9; i++) {
         tabella[i]=[];
@@ -23,8 +41,27 @@ function generaTabella() {
         for (let j=0; j<10; j++) {
             tabella[i][j]=document.createElement("td");
             tabella[i][j].className="celle";
-            tabella[i][j].textContent=`${numeri[i*10+j]}`;
+            tabella[i][j].textContent=`${num[c]}`;
             griglia.appendChild(tabella[i][j]);
+            c++;
         }
     }
+    return tabella;
+}
+
+function CreaInput(output) {
+    let input=document.createElement("input");
+    input.type="number";
+    input.id="numero";
+    input.min="1";
+    input.max="90";
+    input.placeholder="Es.: 45";
+    output.appendChild(input);
+
+    let button=document.createElement("button");
+    button.textContent="Invia numero";
+    button.onclick="ControlloNumerico()";
+    output.appendChild(button);
+
+    return;
 }
